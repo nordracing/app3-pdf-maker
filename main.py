9b176cd461ec4e286f8f4482fc020e5deeb866fb
igorl <igor.litviennco@gmail.com>
2024-11-28 09:41:00 +0200

    Add lines, day 24

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21, 200,21)
+    for y in range(20,297,10):
+        pdf.line(10,y, 200,y)
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -21,7 +22,8 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-
+        for y in range(10,297,10):
+            pdf.line(10, y, 200, y)
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
